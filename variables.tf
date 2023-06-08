fc21c67f46efeb127d11a3c1deebcde1fbe804f9
Niro <46478191+niroz89@users.noreply.github.com>
2023-06-08 18:08:35 +0800

    Allow tags to api gateway resource (#16)

--- variables.tf
+++ variables.tf
@@ -149,3 +149,9 @@ variable "resource_policy_json" {
   type        = string
   default     = null
 }
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
