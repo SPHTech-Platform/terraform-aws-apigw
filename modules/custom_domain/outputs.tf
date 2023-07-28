a7ef45ae597a4fba752755a07a82961aba862074
Goh Hong Yi <hygoh@sph.com.sg>
2023-07-28 10:37:26 +0800

    added outputs to allow creation of route53 alias (#22)

--- modules/custom_domain/outputs.tf
+++ modules/custom_domain/outputs.tf
@@ -2,3 +2,13 @@ output "arn" {
   value       = var.endpoint_type == "EDGE" ? aws_api_gateway_domain_name.domain_edge[0].arn : aws_api_gateway_domain_name.domain_regional[0].arn
   description = "ARN of domain name."
 }
+
+output "domain_name" {
+  value       = var.endpoint_type == "EDGE" ? aws_api_gateway_domain_name.domain_edge[0].cloudfront_domain_name : aws_api_gateway_domain_name.domain_regional[0].regional_domain_name
+  description = "hostname for custom domain regional/cloudfront endpoint"
+}
+
+output "hosted_zone_id" {
+  value       = var.endpoint_type == "EDGE" ? aws_api_gateway_domain_name.domain_edge[0].cloudfront_zone_id : aws_api_gateway_domain_name.domain_regional[0].regional_zone_id
+  description = "hosted zone id of custom domain"
+}
