a57678b6912ad1ec25bc3b7df82c965d7945b21d
Jazeel <48310743+jaezeu@users.noreply.github.com>
2023-10-19 16:10:44 +0800

    Create deployment after policy attachment complete (#26)
    * Create deployment after policy attachment complete
    
    * fmt
    
    * Add depends on on top
    
    ---------
    
    Co-authored-by: Jazeel <jazeel@sph.com.sg>

--- main.tf
+++ main.tf
@@ -19,6 +19,7 @@ resource "aws_api_gateway_rest_api" "api" {
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
+  depends_on  = [aws_api_gateway_rest_api_policy.policy_attachment]
   rest_api_id = aws_api_gateway_rest_api.api.id
 
   triggers = {
