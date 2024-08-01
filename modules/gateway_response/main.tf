e38917762518068267c2eb23e7be34ef4e4e1964
Goh Hong Yi <hygoh@sph.com.sg>
2024-08-01 14:32:25 +0800

    use response type as key

--- modules/gateway_response/main.tf
+++ modules/gateway_response/main.tf
@@ -2,8 +2,8 @@ resource "aws_api_gateway_gateway_response" "wrapper" {
   for_each = var.gateway_responses
 
   rest_api_id   = var.rest_api_id
-  status_code   = each.key
-  response_type = each.value.response_type
+  response_type = each.key
+  status_code   = each.value.status_code
 
   response_templates  = each.value.response_templates
   response_parameters = each.value.response_parameters
