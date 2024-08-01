9c732bc0135bcd079265ca8873f2da3f43e46a73
Goh Hong Yi <hygoh@sph.com.sg>
2024-08-01 13:30:26 +0800

    use status code as key

--- modules/gateway_response/main.tf
+++ modules/gateway_response/main.tf
@@ -2,7 +2,7 @@ resource "aws_api_gateway_gateway_response" "wrapper" {
   for_each = var.gateway_responses
 
   rest_api_id   = var.rest_api_id
-  status_code   = each.value.status_code
+  status_code   = each.key
   response_type = each.value.response_type
 
   response_templates  = each.value.response_templates
