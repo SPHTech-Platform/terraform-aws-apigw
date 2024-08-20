e15f3fc598c8c3d5e75da00866d7b02765e72927
Goh Hong Yi <hygoh@sph.com.sg>
2024-08-20 13:06:31 +0800

    make resource standalone, remove for loop (#32)

--- modules/gateway_response/main.tf
+++ modules/gateway_response/main.tf
@@ -1,10 +1,8 @@
-resource "aws_api_gateway_gateway_response" "wrapper" {
-  for_each = var.gateway_responses
-
+resource "aws_api_gateway_gateway_response" "this" {
   rest_api_id   = var.rest_api_id
-  response_type = each.key
-  status_code   = each.value.status_code
+  response_type = var.response_type
+  status_code   = var.status_code
 
-  response_templates  = each.value.response_templates
-  response_parameters = each.value.response_parameters
+  response_templates  = var.gateway_responses
+  response_parameters = var.response_parameters
 }
