2d75b9a9fba4f21e41fbe941584efd4cb935ec0d
Goh Hong Yi <hygoh@sph.com.sg>
2024-08-20 13:01:19 +0800

    make resource standalone, remove for loop

--- modules/gateway_response/variables.tf
+++ modules/gateway_response/variables.tf
@@ -4,8 +4,26 @@ variable "rest_api_id" {
   default     = ""
 }
 
+variable "response_type" {
+  description = "The response type of the gateway response"
+  type        = string
+  default     = ""
+}
+
+variable "status_code" {
+  description = "The status code of the gateway response"
+  type        = string
+  default     = ""
+}
+
 variable "gateway_responses" {
   description = "map of gateway responses"
+  type        = map(any)
+  default     = {}
+}
+
+variable "response_parameters" {
+  description = "map of response parameters"
   type        = map(any)
   default     = {}
 }
