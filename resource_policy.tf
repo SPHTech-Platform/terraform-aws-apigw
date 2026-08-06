06ce2cfd6b4880db0fac29514dbe4f856e4d467c
Uchinda Padmaperuma <89894943+uchinda-sph@users.noreply.github.com>
2026-08-06 10:56:27 +0800

    [PES-2398] Fix: Re-apply API Gateway policy on OpenAPI spec update (#41)
    * [PES-2398] Fix: Re-apply API Gateway policy on OpenAPI spec update
    
    * chore: remove trailing newlines and add local tracking directories and documentation to gitignore

--- resource_policy.tf
+++ resource_policy.tf
@@ -3,4 +3,10 @@ resource "aws_api_gateway_rest_api_policy" "policy_attachment" {
 
   rest_api_id = aws_api_gateway_rest_api.api.id
   policy      = var.resource_policy_json
+
+  lifecycle {
+    replace_triggered_by = [
+      aws_api_gateway_rest_api.api.body
+    ]
+  }
 }
