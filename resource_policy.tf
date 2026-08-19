ae25de484b6e2282e62b88ff14dd74261cf83674
Uchinda Padmaperuma <89894943+uchinda-sph@users.noreply.github.com>
2026-08-19 10:58:03 +0800

    [PES-2398] Fix: Disable create_before_destroy on policy attachment to prevent post-deployment policy deletion (#42)
    * [PES-2402] Fix: Use terraform_data replacement trigger for API Gateway policy attachment
    
    * chore: remove trailing newline from resource_policy.tf
    
    * [PES-2398] Fix: Set create_before_destroy = false on policy_attachment to prevent post-deployment policy deletion

--- resource_policy.tf
+++ resource_policy.tf
@@ -1,3 +1,9 @@
+resource "terraform_data" "body_change_trigger" {
+  triggers_replace = [
+    sha256(var.body_template)
+  ]
+}
+
 resource "aws_api_gateway_rest_api_policy" "policy_attachment" {
   count = var.enable_resource_policy ? 1 : 0
 
@@ -5,8 +11,9 @@ resource "aws_api_gateway_rest_api_policy" "policy_attachment" {
   policy      = var.resource_policy_json
 
   lifecycle {
+    create_before_destroy = false
     replace_triggered_by = [
-      aws_api_gateway_rest_api.api.body
+      terraform_data.body_change_trigger
     ]
   }
 }
