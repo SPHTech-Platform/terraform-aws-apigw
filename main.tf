9e893d55ab22cc02dce24d59c32be347ffae7aff
Uchinda Padmaperuma <uchinda@sph.com.sg>
2026-08-21 15:25:07 +0800

    [PES-2403] Fix: Add explicit depends_on on REST API resource to prevent Scalr apply race condition

--- main.tf
+++ main.tf
@@ -42,6 +42,10 @@ resource "aws_api_gateway_deployment" "deployment" {
     ))
   }
 
+  depends_on = [
+    aws_api_gateway_rest_api.api
+  ]
+
   lifecycle {
     create_before_destroy = true
   }
